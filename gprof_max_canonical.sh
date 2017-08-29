5b815657e88b9e6b8b25e85616e2748f50bc9ec3
James Trimble <james.trimble@yahoo.co.uk>
2017-08-29 15:15:32 +0100

    Fixed profiling script

--- gprof_max_canonical.sh
+++ gprof_max_canonical.sh
@@ -1,3 +1,3 @@
-gcc -O3 -g -ggdb -pg -Wall -o ex_max_prof ex_max_canonical_deletions.c graph_plus2.c graph_util.c expected_values.c util.c nautyL1.a
+gcc -O3 -g -ggdb -pg -Wall -o ex_max_prof ex_max_canonical_deletions.c graph_plus.c graph_util.c util.c possible_augmentations.c nautyL1.a
 ./ex_max_prof 5 32 85 | tail
 gprof ex_max_prof gmon.out > prof_output

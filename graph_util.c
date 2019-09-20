1a1b96cbd0da0430fbb8d9bedac471917a8e63e1
James Trimble <james.trimble@yahoo.co.uk>
2019-09-20 21:25:15 +0100

    A micro-optimisation to short path check

--- graph_util.c
+++ graph_util.c
@@ -40,23 +40,25 @@ int num_neighbours_of_deg_d(graph *g, int v, int d, int *degs) {
 void all_pairs_check_for_short_path(graph *g, int n, int max_path_len, setword *have_short_path)
 {
     for (int i=0; i<n; i++) {
-        have_short_path[i] = g[i];
+        have_short_path[i] = g[i] | bit[i];
         ADDELEMENT(&have_short_path[i], i);
     }
 
     for (int k=2; k<=max_path_len; k++) {
         for (int i=1; i<n; i++) {
-            graph tmp = have_short_path[i];
+            graph have_short_path_i = have_short_path[i];   // micro-optimisation
+            graph tmp = have_short_path_i;
             for (int j=0; j<i; j++) {
                 //  The next few lines do the same as the following, without an 'if'
                 //  if (tmp & g[j]) {
-                //      ADDELEMENT(&have_short_path[i], j);
+                //      ADDELEMENT(&have_short_path_i, j);
                 //      ADDELEMENT(&have_short_path[j], i);
                 //  }
                 bool have_path = ((tmp & g[j]) != 0ull);
-                have_short_path[i] |= have_path * bit[j];
+                have_short_path_i |= have_path * bit[j];
                 have_short_path[j] |= have_path * bit[i];
             }
+            have_short_path[i] = have_short_path_i;
         }
     }
 }

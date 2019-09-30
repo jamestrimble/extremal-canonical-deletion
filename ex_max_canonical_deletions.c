d8abc6f90bb5a1911fd48d6ba479d4c97087aaa2
James Trimble <james.trimble@yahoo.co.uk>
2019-09-30 16:29:17 +0100

    Re-order checks in tentative version

--- ex_max_canonical_deletions.c
+++ ex_max_canonical_deletions.c
@@ -224,31 +224,29 @@ bool output_graph(struct SearchData *sd, setword neighbours, bool max_deg_increm
     int min_deg = degs[n-1];
     int max_deg = sd->gp->max_deg + max_deg_incremented;
     if (sd->tentative_version) {
-        if (deletion_is_canonical(new_g, n, min_deg, max_deg, degs, true)) {
-            int num_of_min_deg;
-            if (min_deg == sd->gp->min_deg) {
-                num_of_min_deg = 1 + POPCOUNT(sd->vertices_of_min_deg & ~neighbours);
-            } else {
-                num_of_min_deg = 0;
-                for (int i=0; i<n; i++)
-                    if (degs[i] == min_deg)
-                        ++num_of_min_deg;
-            }
-            setword min_degs = 0;
-            for (int increment_max_deg_again=0; increment_max_deg_again<2; increment_max_deg_again++) {
-                struct GraphType * gt = find_graph_type_in_set(&(struct GraphType) {
-                            .num_vertices=n + 1,
-                            .num_edges_minus_min_deg=sd->gp->edge_count + min_deg,
-                            .max_deg=max_deg + increment_max_deg_again
-                        });
-                if (gt)
-                    min_degs |= gt->min_degs;
-            }
-            int top = num_of_min_deg > min_deg + 1 ? min_deg : min_deg + 1;
-            for (int i=0; i<=top; i++)
-                if (ISELEMENT(&min_degs, i))
-                    return true;
+        int num_of_min_deg;
+        if (min_deg == sd->gp->min_deg) {
+            num_of_min_deg = 1 + POPCOUNT(sd->vertices_of_min_deg & ~neighbours);
+        } else {
+            num_of_min_deg = 0;
+            for (int i=0; i<n; i++)
+                if (degs[i] == min_deg)
+                    ++num_of_min_deg;
         }
+        setword min_degs = 0;
+        for (int increment_max_deg_again=0; increment_max_deg_again<2; increment_max_deg_again++) {
+            struct GraphType * gt = find_graph_type_in_set(&(struct GraphType) {
+                        .num_vertices=n + 1,
+                        .num_edges_minus_min_deg=sd->gp->edge_count + min_deg,
+                        .max_deg=max_deg + increment_max_deg_again
+                    });
+            if (gt)
+                min_degs |= gt->min_degs;
+        }
+        int top = num_of_min_deg > min_deg + 1 ? min_deg : min_deg + 1;
+        for (int i=0; i<=top; i++)
+            if (ISELEMENT(&min_degs, i))
+                return deletion_is_canonical(new_g, n, min_deg, max_deg, degs, true);
         return false;
     } else if (deletion_is_canonical(new_g, n, min_deg, max_deg, degs, false)) {
         struct GraphPlus tentative_gp;

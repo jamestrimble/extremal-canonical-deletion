ee1ad338127d730ad0edc0d48752b67d0bfbbce8
James Trimble <james.trimble@yahoo.co.uk>
2019-10-02 09:34:56 +0100

    Add compiler flags to profiling script

--- gprof_max_canonical.sh
+++ gprof_max_canonical.sh
@@ -1,3 +1,3 @@
-gcc -O3 -g -ggdb -pg -Wall -o ex_max_prof ex_max_canonical_deletions.c graph_plus.c graph_util.c util.c possible_graph_types.c nautyL1.a
+gcc -O3 -g -ggdb -pg -Wall -o ex_max_prof ex_max_canonical_deletions.c graph_plus.c graph_util.c util.c possible_graph_types.c nautyL1.a -march=native -mpopcnt
 ./ex_max_prof 5 32 85 | tail
 gprof ex_max_prof gmon.out > prof_output

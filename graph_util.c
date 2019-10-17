87f3bbd99c306a6c53973fd9544d594503bba1f8
James Trimble <james.trimble@yahoo.co.uk>
2019-10-17 21:04:15 +0100

    Move canonicalisation out of main file

--- graph_util.c
+++ graph_util.c
@@ -74,3 +74,220 @@ void show_graph(struct GraphPlus *gp)
     printf("\n");
 }
 
+////////////////////////////////////////////////////////////////////////////////
+//  Canonicalisation
+////////////////////////////////////////////////////////////////////////////////
+struct VtxInfo
+{
+    int v;
+    int deg;
+    unsigned long long nnds;
+};
+
+#define INSERTION_SORT(type, arr, arr_len, swap_condition) do { \
+    for (int i=1; i<arr_len; i++) {                             \
+        for (int j=i; j>=1; j--) {                              \
+            if (swap_condition) {                               \
+                type tmp = arr[j-1];                            \
+                arr[j-1] = arr[j];                              \
+                arr[j] = tmp;                                   \
+            } else {                                            \
+                break;                                          \
+            }                                                   \
+        }                                                       \
+    }                                                           \
+} while(0);
+
+bool compare_vtx_info(struct VtxInfo *vi0, struct VtxInfo *vi1)
+{
+    if (vi0->deg < vi1->deg)
+        return true;
+    if (vi0->deg > vi1->deg)
+        return false;
+    if (vi0->nnds < vi1->nnds)
+        return true;
+    return false;
+}
+
+static int vtx_to_orbit[MAXN];
+static setword orbits[MAXN];
+static int incumbent_order[MAXN];
+
+void possibly_update_incumbent(graph *g, int n, int *order, int order_len,
+        setword *vv_set, int num_sets, graph *incumbent_g)
+{
+    for (int i=0; i<num_sets; i++)
+        order[order_len++] = FIRSTBITNZ(vv_set[i]);
+
+    int order_inv[MAXN];
+    for (int i=0; i<n; i++)
+        order_inv[order[i]] = i;
+
+    graph new_g[MAXN] = {};
+    for (int i=0; i<n; i++) {
+        setword row = g[order[i]];
+        while (row) {
+            int w;
+            TAKEBIT(w, row);
+            ADDELEMENT(&new_g[i], order_inv[w]);
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        if (new_g[i] < incumbent_g[i]) {
+            for (int j=0; j<n; j++)
+                incumbent_g[j] = new_g[j];
+            for (int j=0; j<n; j++)
+                incumbent_order[j] = order[j];
+            return;
+        } else if (new_g[i] > incumbent_g[i]) {
+            return;
+        }
+    }
+
+    // The graph is the same as the incumbent.  Update orbits.
+    for (int i=0; i<n; i++) {
+        int v = order[i];
+        int w = incumbent_order[i];
+        int orb_v = vtx_to_orbit[v];
+        int orb_w = vtx_to_orbit[w];
+        if (orb_v != orb_w) {
+            // combine orbits of v and w
+            setword s = orbits[orb_w];
+            while (s) {
+                int u;
+                TAKEBIT(u, s);
+                vtx_to_orbit[u] = orb_v;
+            }
+            orbits[orb_v] |= orbits[orb_w];
+        }
+    }
+}
+
+bool only_singleton_sets_exist(setword *vv_set, int num_sets)
+{
+    for (int i=0; i<num_sets; i++)
+        if (POPCOUNT(vv_set[i]) > 1)
+            return false;
+    return true;
+}
+
+int choose_set_for_splitting(setword *vv_set, int num_sets)
+{
+    int min_set_len = 99999;
+    int best_set_idx = -1;
+    for (int i=0; i<num_sets; i++) {
+        int len = POPCOUNT(vv_set[i]);
+        if (len < min_set_len) {
+            min_set_len = len;
+            best_set_idx = i;
+            if (len == 1)
+                break;    // just to save time
+        }
+    }
+    return best_set_idx;
+}
+
+void canon_search(graph *g, graph *incumbent_g, int n,
+        setword *vv_set, int num_sets, int *order, int order_len)
+{
+    //symmetry breaking
+    if (incumbent_g[0] != ~0ull) {   // only if incumbent has been set
+        for (int i=0; i<order_len; i++) {
+            int v = order[i];
+            int incumbent_v = incumbent_order[i];
+            if (v != incumbent_v && ISELEMENT(&orbits[vtx_to_orbit[v]], incumbent_v)) {
+                return;
+            }
+            if (vtx_to_orbit[v] != v || POPCOUNT(orbits[v]) != 1) {
+                break;
+            }
+        }
+    }
+    if (order_len > 0 && (incumbent_g[0] != ~0ull) && (order[0] != incumbent_order[0]) &&
+            ISELEMENT(&orbits[vtx_to_orbit[order[0]]], incumbent_order[0]))
+        return;
+
+    if (only_singleton_sets_exist(vv_set, num_sets)) {
+        possibly_update_incumbent(g, n, order, order_len, vv_set, num_sets, incumbent_g);
+        return;
+    }
+
+    int best_set_idx = choose_set_for_splitting(vv_set, num_sets);
+
+    setword vv = vv_set[best_set_idx];
+    setword visited = 0;
+    while (vv) {
+        int w;
+        TAKEBIT(w, vv);
+        vv_set[best_set_idx] ^= bit[w];   // temporarily remove w
+        setword new_vv_set[MAXN];
+        int new_num_sets = 0;
+        for (int j=0; j<num_sets; j++) {
+            // McSplit-style splitting
+            setword a = vv_set[j] & g[w];
+            setword b = vv_set[j] & ~g[w];
+            if (a != 0)
+                new_vv_set[new_num_sets++] = a;
+            if (b != 0)
+                new_vv_set[new_num_sets++] = b;
+        }
+        order[order_len] = w;
+        canon_search(g, incumbent_g, n, new_vv_set, new_num_sets, order, order_len+1);
+        vv_set[best_set_idx] ^= bit[w];   // add w back
+        ADDELEMENT(&visited, w);
+    }
+}
+
+void make_canonical(graph *g, int n, graph *canon_g)
+{
+#ifdef SELF_CONTAINED
+    for (int i=0; i<n; i++) {
+        vtx_to_orbit[i] = i;
+        orbits[i] = bit[i];
+    }
+
+    int degs[MAXN];
+    for (int i=0; i<n; i++)
+        degs[i] = POPCOUNT(g[i]);
+
+    struct VtxInfo vtx_info[MAXN];
+    for (int i=0; i<n; i++)
+        vtx_info[i] = (struct VtxInfo) {i, degs[i], weighted_nb_nb_deg_sum(g, i, degs)};
+
+    INSERTION_SORT(struct VtxInfo, vtx_info, n, compare_vtx_info(&vtx_info[j], &vtx_info[j-1]));
+
+    setword vv_set[MAXN];
+    struct VtxInfo prev_vtx_info = (struct VtxInfo) {-1, -1, 0};
+    int current_set_num = -1;
+    for (int i=0; i<n; i++) {
+        if (vtx_info[i].deg != prev_vtx_info.deg || vtx_info[i].nnds != prev_vtx_info.nnds) {
+            ++current_set_num;
+            vv_set[current_set_num] = 0;
+        }
+        vv_set[current_set_num] |= bit[vtx_info[i].v];
+        prev_vtx_info = vtx_info[i];
+    }
+    int num_sets = current_set_num + 1;
+
+    graph incumbent_g[MAXN] = {};
+    for (int i=0; i<n; i++)
+        incumbent_g[i] = ~0ull;
+    int order[MAXN];
+    canon_search(g, incumbent_g, n, vv_set, num_sets, order, 0);
+
+    for (int i=0; i<n; i++)
+        canon_g[i] = incumbent_g[i];
+
+#else
+    static DEFAULTOPTIONS_GRAPH(options);
+    options.getcanon = TRUE;
+    options.tc_level = 0;
+    static statsblk stats;
+    int lab[MAXN],ptn[MAXN],orbits[MAXN];
+    EMPTYGRAPH(canon_g,1,MAXN);
+    setword workspace[120];
+    nauty(g,lab,ptn,NULL,orbits,&options,&stats,workspace,120,1,n,canon_g);
+#endif
+}
+
--- graph_util.h
+++ graph_util.h
@@ -9,3 +9,5 @@ int num_neighbours_of_deg_d(graph *g, int v, int d, int *degs);
 void all_pairs_check_for_short_path(graph *g, int n, int max_path_len, setword *have_short_path);
 
 void show_graph(struct GraphPlus *gp);
+
+void make_canonical(graph *g, int n, graph *canon_g);
